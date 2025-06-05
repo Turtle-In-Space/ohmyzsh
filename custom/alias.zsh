382f0e2d3c99d0701f4aab129d04d28cb6ecb0f7
Turtle-In-Space <elias.svensson63@gmail.com>
2025-06-05 08:45:05 +0200

    add aliases for eza and bat

---
+++ custom/alias.zsh
@@ -0,0 +1,5 @@
+alias cat="bat"
+
+alias ls="eza --sort=type --group-directories-first"
+alias ll="eza --sort=type --group-directories-first --long --almost-all --header --smart-group --no-filesize --no-user --no-time"
+alias lt="eza --sort=type --group-directories-first --tree --level=3"
