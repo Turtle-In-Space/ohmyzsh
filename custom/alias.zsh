66315a77637a0531e938ff5dbacb8f7386940a59
Turtle-In-Space <elias.svensson63@gmail.com>
2025-06-05 12:39:24 +0200

    add icons to ls

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,5 +1,5 @@
 alias cat="bat"
 
-alias ls="eza --sort=type --group-directories-first"
-alias ll="eza --sort=type --group-directories-first --long --almost-all --header --smart-group --no-filesize --no-user --no-time"
-alias lt="eza --sort=type --group-directories-first --tree --level=3"
+alias ls="eza --sort=type --group-directories-first --icons=always "
+alias ll="eza --sort=type --group-directories-first --icons=always --long --almost-all --header --smart-group --no-filesize --no-user --no-time"
+alias lt="eza --sort=type --group-directories-first --icons=always --tree --level=3"
